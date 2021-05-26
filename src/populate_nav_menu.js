3b614a7885d1aa24c2e296144819f29c90b28cd7
cooksoundscapes <me@dina.localdomain>
2021-05-26 18:11:04 -0300

    Committer: cooksoundscapes <me@dina.localdomain>
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   index.html
    	modified:   src/populate_nav_menu.js

--- src/populate_nav_menu.js
+++ src/populate_nav_menu.js
@@ -9,4 +9,3 @@ const menu = document.querySelector('.nav_menu');
 menu.appendChild(createNumberBox('BPM:','set_bpm',[30,600],setBPM,120));
 menu.appendChild(createSlider('Master: ','master_volume',[0,1],0.005,setMasterVol,1));
 menu.appendChild(createSpan('0.00dB','decibels'));
-menu.appendChild(createSpan('IDB test','idbtest'));
